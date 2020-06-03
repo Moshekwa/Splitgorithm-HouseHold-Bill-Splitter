26c87deb9ff5ed67d8e4f7e8e6b16e980aaa44b2
Lungisani Siphesihle Luvuno <1478317@students.wits.ac.za>
2020-06-03 02:57:56 +0200

    Modifying all necessary files to ensure that everything is in order

--- splitgorithmRoutes.js
+++ splitgorithmRoutes.js
@@ -4,4 +4,12 @@ const path = require('path')
 const express = require('express')
 const router = express.Router()
 
+router.get('/signUpPage', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'class', 'signUpPage.html'))
+})
+
+router.post('/api/signUpPage', function (req, res) {
+  console.log('Signing up the following member:', req.body.name)
+})
+
 module.exports = router
