6eb06ae59ff157a5b0a6c1add214c662a0d1519c
Lungisani Siphesihle Luvuno <1478317@students.wits.ac.za>
2020-06-12 19:42:16 +0200

    Hashing user passwords using the npm-bcrypt

--- splitgorithmRoutes.js
+++ splitgorithmRoutes.js
@@ -3,6 +3,8 @@
 const path = require('path')
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const db = require('./db.js')
 
 // members in a house hold
 const members = require('./modules/members.js')
@@ -76,6 +78,33 @@ router.post('/api/signup', function (req, res) {
     members.addMember(memberObject)
     res.redirect(req.baseUrl + '/homepage')
   } else res.redirect(req.baseUrl + '/signup')
+
+  db.sql.connect(db.getConfig())
+    .then(() => {
+      console.log('connected')
+
+      const table = new db.sql.Table('SplitgorithmUsers')
+      table.create = true
+      table.columns.add('name', db.sql.VarChar(128), { nullable: false })
+      table.columns.add('username', db.sql.VarChar(128), { nullable: false, primary: true })
+      table.columns.add('email', db.sql.VarChar(128), { nullable: false })
+      table.columns.add('password', db.sql.VarChar(128), { nullable: false })
+
+      const salt = bcrypt.genSaltSync(10)
+      const hashedPassword = bcrypt.hashSync(members.getMember(members.getMembers().length - 1).password, salt)
+
+      // adding a member in a table
+      table.rows.add(members.getMember(members.getMembers().length - 1).name, members.getMember(members.getMembers().length - 1).username,
+        members.getMember(members.getMembers().length - 1).email, hashedPassword)
+      const request = new db.sql.Request()
+      return request.bulk(table)
+    })
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
 })
 
 router.get('/welcome', function (req, res) {
