e43eec9d21c11597caea9af0cfeb0159f981aabf
moshekwa <1387556@students.wits.ac.za>
2020-06-22 22:55:42 +0200

    Added cookies to sessions

--- index.js
+++ index.js
@@ -12,11 +12,30 @@ const todoRouter = require('./splitgorithmRoutes.js')
 // loading express sessions dependency
 const session = require('express-session')
 
+const TWO_HOURS = 60 * 60 * 2 * 1000; // The lifespan of a cookie 
+const { 
+    SESS_NAME = 'userSession',
+    SESS_SECRET = 'splitgorithmSessions',
+    SESS_LIFETIME = TWO_HOURS,
+    NODE_ENV = 'development'
+} = process.env
+const IN_PROD = NODE_ENV === 'production' // Production enviroment could be set to deveopment 
+
 // tell express to use bodyParser for JSON and URL encoded form bodies
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 //app.use(session({ secret: process.env.SESSION_SECRET )
-app.use(session({secret: "mateu", resave: false, saveUninitialized: true}));
+app.use(session({
+name: SESS_NAME,
+secret: SESS_SECRET, 
+resave: false, 
+saveUninitialized: false, 
+cookie : {
+    maxAge: SESS_LIFETIME,
+    sameSite: true,
+    secure: IN_PROD
+}
+}));
 
 // mounting our routers
 app.use('/', mainRouter)
--- splitgorithmRoutes.js
+++ splitgorithmRoutes.js
@@ -25,6 +25,8 @@ router.get('/signup', function (req, res) {
 })
 
 router.get('/homepage', function (req, res) {
+  
+  console.log(req.sessionID)
   if (req.session.loggedIn)
   { 
     console.log('The session has been set')
